17c66b5e1e00ce5ae9206c609fbd1c044ae385fb
Christophe Beyer <cbeyer@free.fr>
2023-09-18 00:05:39 +0200

    Update PublicForTestsCompilerPass.php

--- DependencyInjection/Compiler/PublicForTestsCompilerPass.php
+++ DependencyInjection/Compiler/PublicForTestsCompilerPass.php
@@ -9,7 +9,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class PublicForTestsCompilerPass implements CompilerPassInterface
 {
-    public function process(ContainerBuilder $container)
+    public function process(ContainerBuilder $container): void
     {
         if (!$this->isPHPUnit()) {
             return;
